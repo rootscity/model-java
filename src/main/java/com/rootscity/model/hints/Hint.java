b162d120e5635787578ef37af48f98d581a73631
Daniel Segui <dsegui.v@gmail.com>
2017-04-26 16:40:30 -0300

    Clean a fmp remmanent, change List to Set on hintToken.hints to avoid duplicated hints

--- src/main/java/com/rootscity/model/hints/Hint.java
+++ src/main/java/com/rootscity/model/hints/Hint.java
@@ -12,10 +12,59 @@ public class Hint {
   public String eventPlace;       // optional
   public String eventType;        // optional - human (textual) description
 
-  private transient String pBaseUri = "http://search.findmypast.com/record?id=";
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
 
-  public void setRecordId(String rId) {
-    recordId = rId;
-    personaUri = pBaseUri + rId;
+    Hint hint = (Hint) o;
+
+    if (personaUri != null ? !personaUri.equals(hint.personaUri) : hint.personaUri != null) {
+      return false;
+    }
+    if (personaName != null ? !personaName.equals(hint.personaName) : hint.personaName != null) {
+      return false;
+    }
+    if (personaTitle != null ? !personaTitle.equals(hint.personaTitle) : hint.personaTitle != null) {
+      return false;
+    }
+    if (personaId != null ? !personaId.equals(hint.personaId) : hint.personaId != null) {
+      return false;
+    }
+    if (recordId != null ? !recordId.equals(hint.recordId) : hint.recordId != null) {
+      return false;
+    }
+    if (collectionTitle != null ? !collectionTitle.equals(hint.collectionTitle) : hint.collectionTitle != null) {
+      return false;
+    }
+    if (confidence != null ? !confidence.equals(hint.confidence) : hint.confidence != null) {
+      return false;
+    }
+    if (eventYear != null ? !eventYear.equals(hint.eventYear) : hint.eventYear != null) {
+      return false;
+    }
+    if (eventPlace != null ? !eventPlace.equals(hint.eventPlace) : hint.eventPlace != null) {
+      return false;
+    }
+    return eventType != null ? eventType.equals(hint.eventType) : hint.eventType == null;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = personaUri != null ? personaUri.hashCode() : 0;
+    result = 31 * result + (personaName != null ? personaName.hashCode() : 0);
+    result = 31 * result + (personaTitle != null ? personaTitle.hashCode() : 0);
+    result = 31 * result + (personaId != null ? personaId.hashCode() : 0);
+    result = 31 * result + (recordId != null ? recordId.hashCode() : 0);
+    result = 31 * result + (collectionTitle != null ? collectionTitle.hashCode() : 0);
+    result = 31 * result + (confidence != null ? confidence.hashCode() : 0);
+    result = 31 * result + (eventYear != null ? eventYear.hashCode() : 0);
+    result = 31 * result + (eventPlace != null ? eventPlace.hashCode() : 0);
+    result = 31 * result + (eventType != null ? eventType.hashCode() : 0);
+    return result;
   }
 }
--- src/main/java/com/rootscity/model/hints/HintToken.java
+++ src/main/java/com/rootscity/model/hints/HintToken.java
@@ -1,7 +1,7 @@
 package com.rootscity.model.hints;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class HintToken {
 
@@ -10,7 +10,7 @@ public class HintToken {
   public String fsTreeId;         // the id of the associated tree in FamilySearch
   public Long personId;           // RootsFinder person id
   public String fsPersonUri;      // URI of the associated person in FamilySearch Genealogies
-  public List<Hint> hints = new ArrayList<>();
+  public Set<Hint> hints = new HashSet<>();
 
 	public void addHint(Hint hint) {
 		hints.add(hint);
