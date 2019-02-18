bd3723a182ed72a207d50ce787247f78e340f0ca
Dallan Quass <dallan@quass.org>
2019-02-18 14:20:40 +0000

    add treeSource

--- src/main/java/com/rootscity/model/hints/HintToken.java
+++ src/main/java/com/rootscity/model/hints/HintToken.java
@@ -7,6 +7,7 @@ public class HintToken {
 
   public String source;           // "FamilySearch Historical Records"
   public Long treeId;             // RootsFinder id of the tree that these hints belong to
+  public String treeSource;       // source (RF/FMP) of the tree that these hints belong to
   public String fsTreeId;         // the id of the associated tree in FamilySearch
   public Long personId;           // RootsFinder person id
   public String fsPersonUri;      // URI of the associated person in FamilySearch Genealogies
